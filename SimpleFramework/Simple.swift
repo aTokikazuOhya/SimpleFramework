db9715e543541568b82c47370d37fbbc5930658f
aTokikazuOhya <ohya@aplix.co.jp>
2024-04-26 18:30:34 +0900

    空っぽフレームワーク作成

---
+++ SimpleFramework/Simple.swift
@@ -0,0 +1,15 @@
+//
+//  Simple.swift
+//  SimpleFramework
+//
+//  Created by Tokikazu Ohya on 2024/04/26.
+//
+
+import Foundation
+
+public class Simple {
+    static func main() -> Bool {
+        print("### Simple Framework! ###")
+        return true
+    }
+}
---
+++ SimpleFramework/SimpleFramework.h
@@ -0,0 +1,18 @@
+//
+//  SimpleFramework.h
+//  SimpleFramework
+//
+//  Created by Tokikazu Ohya on 2024/04/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleFramework.
+FOUNDATION_EXPORT double SimpleFrameworkVersionNumber;
+
+//! Project version string for SimpleFramework.
+FOUNDATION_EXPORT const unsigned char SimpleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleFramework/PublicHeader.h>
+
+
